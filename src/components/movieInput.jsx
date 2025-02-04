a4d62276409b14cdb2eed9138c0cb15c478dcd19
athuljain <athuljainkj2@gmail.com>
2025-02-04 11:40:50 +0530

    components commit

---
+++ src/components/movieInput.jsx
@@ -0,0 +1,8 @@
+export const MovieInput = () => {
+    return(
+        <div>
+            <input type="text"  />
+            <button>Add Movie</button>
+        </div>
+    )
+}
---
+++ src/components/movieList.jsx
@@ -0,0 +1,7 @@
+export const MovieList=()=>{
+    return(
+        <div>
+            <h3>Movies List</h3>
+        </div>
+    )
+}
